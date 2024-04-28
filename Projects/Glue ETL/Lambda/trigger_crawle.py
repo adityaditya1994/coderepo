dc0f5d78961ac68c6df75db20dc3f6d87492ddd9
adityasinghal1801 <nakih44612@wuzak.com>
2024-04-28 08:27:29 +0530

    Added crawler eventbridge

--- Projects/Glue ETL/Lambda/trigger_crawle.py
+++ Projects/Glue ETL/Lambda/trigger_crawle.py
@@ -2,9 +2,9 @@ import json
 import boto3
 glue=boto3.client('glue')
 
-def trigger_crawler:
+def trigger_crawler():
     response = glue.start_crawler(
-    Name='{Put the Name of the Glue Crawler here}'
+    Name='ahs-glue-crawler-sales-data-2604'
     )
     return response
 
@@ -14,13 +14,12 @@ def trigger_glue_job():
 
 
 def lambda_handler(event, context):
-    if trigger_type == 'crawler':
-        trigger_crawler
-    elif trigger_type == 'glue_job':
-        trigger_glue_job() 
+
+    if 'Records' in event:
+        print(trigger_crawler())
+    elif event.get('source') == 'aws.glue':
+        print(trigger_glue_job())
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-    return response
-
